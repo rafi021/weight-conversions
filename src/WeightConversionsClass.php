06504f1bedbd42f26f406caaaba68f79ba39ce5d
ibrahim-shuttlebd <ibrahim-shuttlebd@users.noreply.github.com>
2023-07-17 05:55:33 +0000

    Fix styling

--- src/WeightConversionsClass.php
+++ src/WeightConversionsClass.php
@@ -4,16 +4,17 @@ namespace Rafi021\WeightConversions;
 
 class WeightConversionsClass
 {
-    public  static function kilograms(float $kilograms) : self
+    public static function kilograms(float $kilograms): self
     {
-       return new static ($kilograms);
+        return new static ($kilograms);
     }
 
-    public function __construct(protected float $kilograms) {
+    public function __construct(protected float $kilograms)
+    {
         $this->kilograms = $kilograms;
     }
 
-    public function toPounds() : float
+    public function toPounds(): float
     {
         return $this->kilograms * 2.2046;
     }
