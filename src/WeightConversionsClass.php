1ff211419cc439c236e81c86530ec33c0470061f
mahmud ibrahim <ibrahim@shuttlebd.com>
2023-07-17 11:54:40 +0600

    added functionality and test using pest

--- src/WeightConversionsClass.php
+++ src/WeightConversionsClass.php
@@ -4,4 +4,17 @@ namespace Rafi021\WeightConversions;
 
 class WeightConversionsClass
 {
+    public  static function kilograms(float $kilograms) : self
+    {
+       return new static ($kilograms);
+    }
+
+    public function __construct(protected float $kilograms) {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toPounds() : float
+    {
+        return $this->kilograms * 2.2046;
+    }
 }
